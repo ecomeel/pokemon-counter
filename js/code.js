496a8f8ebaa0b3453588b8897c3155c32b2d7fc8
ecomeel <ecomeel@gmail.com>
2023-05-02 01:40:06 +0300

    Created reset btn + styles

--- js/code.js
+++ js/code.js
@@ -1,8 +1,15 @@
-let plusBtn = document.querySelector('.plus-btn');
-let counterText = document.querySelector('.counter');
+let plusBtn = document.querySelector(".btn_plus");
+let resetBtn = document.querySelector('.btn_reset');
+let counterText = document.querySelector(".counter");
 let counter = 0;
-plusBtn.addEventListener('click', function() {
-    counter = counter + 1;
+
+plusBtn.addEventListener("click", function () {
+  counter = counter + 1;
+  counterText.innerHTML = counter;
+});
+
+
+resetBtn.addEventListener('click', function () {
+    counter = 0;
     counterText.innerHTML = counter;
 })
-
